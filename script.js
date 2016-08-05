659ddf22753eb282dc47414b08fb25eb0f7868b7
Paolo Sese <jpaolo.sese@gmail.com>
2016-08-05 08:49:55 -0700

    mapped movement to key ups

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-
-
 $(document).ready(function() {
   console.log("working");
-  $("td").click(function(e) {
-    $(this).css("background-color", "#888888")
-    $(this).text("clicked")
+  // $("td").click(function(e) {
+  //   $(this).css("background-color", "#888888")
+  //   $(this).text("clicked")
+  // });
+  var playerOneCount = 0;
+  var playerTwoCount = 0;
+  $("body").keyup(function(e) {
+    if (e.keyCode === 81 || 113) {
+      playerOneCount += 1;
+      $("#a" + playerOneCount).css("background-color", "#FFF000");
+    }
+    if (e.keyCode === 80 || 112) {
+      playerTwoCount += 1;
+      $("#b" + playerTwoCount).css("background-color", "#000FFF");
+    }
+    // count += 1;
+    // $("td#a" + count).css("background-color", "#777777");
   });
-})
+});
