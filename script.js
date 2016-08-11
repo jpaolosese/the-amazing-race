e82543dfdfaa5737b442136c696da2778e00a436
melicarls <melissacarlson15@gmail.com>
2016-08-11 10:50:28 -0700

    added todos

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ $(document).ready(function() {
   var playerOneCount = 0;
   var playerTwoCount = 0;
   $("body").keyup(function(e) {
-    if (e.keyCode === 81 || 113) {
+    // TODO: This needs to be written differently.
+    // Right now, the code block executes if
+    // e.keyCode === 81 is truthy OR if
+    // 113 is truthy. Numbers > 0 are always truthy in Javascript.
+    if (e.keyCode === 81 || e.keyCode === 113) {
       playerOneCount += 1;
       $("#a" + playerOneCount).css("background-color", "#FFF000");
     }
-    if (e.keyCode === 80 || 112) {
+    if (e.keyCode === 80 || e.keyCode === 112) {
       playerTwoCount += 1;
       $("#b" + playerTwoCount).css("background-color", "#000FFF");
     }
